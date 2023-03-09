f7a94ca59dac7f3c7b40b2cd91d314d9f7e45a4c
Joab Owala <owalajoab@gmail.com>
2023-03-09 14:26:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    name = cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    name = cats.unshift("Bob");
+    
+}
+
+function destructivelyRemoveLastCat(name){
+    name = cats.pop(-1);
+}
+
+function destructivelyRemoveFirstCat(name){
+    name = cats.shift(0);
+}
+
+function appendCat(name){
+    name = [...cats,"Broom"];
+    return name;
+}
+
+function prependCat(name){
+    name = ["Arnold", ...cats];
+    return name;
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+
+    return cats.slice(1);
+}
